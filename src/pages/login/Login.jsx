9cd5b73a24c001bc587466f8edcc4fb9964c92c7
NABD42 <cp667ekip@gmail.com>
2022-03-21 20:39:35 +0100

    StreamingCompletVF

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,37 @@
+import "./login.scss"
+
+
+export default function Login() {
+  return (
+    <div className="container">
+        <div className="header">
+            <img className="StreamingCompletVF" src="https://cdn.worldvectorlogo.com/logos/microsoft-stream.svg" alt="" />
+            <h1>StreamingCompletVF</h1>
+        </div>
+        <div className="login-box">
+            <div className="icon">
+                <a href="#"><img src={"https://icons-for-free.com/iconfiles/png/512/person+profile+user+icon-1320184018411209468.png"} alt="" /></a>
+            </div>
+            <div className="textbox">
+                <input type="text" autoComplete="email" placeholder="nom d'utilisateur ou email"/>
+            </div>
+            <div className="textbox">
+                <input type="password" placeholder="mot de passe"/>
+            </div>
+            <div className="forgot">
+                <a href="#">
+                    <p>Mot de passe oublié ?</p>
+                </a>
+            </div>
+            <div>
+                <input type="button" className="button" value={"Connexion"}/>
+            </div>
+            <div className="loginbtn">
+                <p>Vous n'avez pas de compte ? <span>
+                   <a href="#">Inscrivez vous.</a> 
+                </span></p>
+            </div>
+        </div>
+    </div>
+  )
+}
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -8,27 +8,24 @@ export default function Register() {
             <img className="StreamingCompletVF" src="https://cdn.worldvectorlogo.com/logos/microsoft-stream.svg" alt="" />
             <h1>StreamingCompletVF</h1>
         </div>
-        <div className="login-box">
-            <div className="icon">
-                <img src={"https://icons-for-free.com/iconfiles/png/512/person+profile+user+icon-1320184018411209468.png"} alt="" />
+        <div className="register-box">
+            <h2>Inscrivez-vous <span className="wordFree">gratuitement !</span></h2>
+            <p>Créez vous un compte et profiter de toutes les <span className="textCentering">exclusivités sans pub et en illimité.</span></p>
+            <div className="registertextbox">
+                <input type="text" placeholder="nom d'utilisateur" required/>
             </div>
-            <div className="textbox">
-                <input type="text" autoComplete="email" placeholder="nom d'utilisateur ou email"/>
+            <div className="registertextbox">
+                <input type="email" placeholder="email" required/>
             </div>
-            <div className="textbox">
+            <div className="registertextbox">
                 <input type="password" placeholder="mot de passe"/>
             </div>
-            <div className="forgot">
-                <a href="#">
-                    <p>Mot de passe oublié ?</p>
-                </a>
-            </div>
             <div>
-                <input type="button" className="button" value={"Connexion"}/>
+                <input type="button" className="button" value={"Inscription"}/>
             </div>
             <div className="signup">
-                <p>Vous n'avez pas de compte ? <span>
-                   <a href="#">Inscrivez vous.</a> 
+                <p>Vous avez déjà un compte? <span>
+                   <a href="#">Connectez vous.</a> 
                 </span></p>
             </div>
         </div>
